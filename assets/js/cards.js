30806536b404bae674ee160f7383f8c718c7d8d5
Livia <livinha.guimaraes.013@gmail.com>
2022-10-01 13:53:09 -0300

    change switch artwork  method

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -64,86 +64,107 @@ let cards = [
     },
     {
         id: 10,
+        name: 'Little_Devil\'s_Sweet_Temptation',
+        img: 'assets/img/cards/3-stars/Little_Devil\'s_Sweet_Temptation.png',
+        rarity: 3,
+        trained: 'assets/img/cards/3-stars/Little_Devil\'s_Sweet_Temptation_T.png'
+    },
+    {
+        id: 11,
         name: 'Birthday Aoyagi Tsubaki',
         img: 'assets/img/cards/4-stars/Birthday_Aoyagi_Tsubaki.png',
         rarity: 4,
         trained: 'assets/img/cards/4-stars/Birthday_Aoyagi_Tsubaki_T.png'
     },
     {
-        id: 11,
+        id: 12,
         name: 'Birthday -Museum- Aoyagi Tsubaki',
         img: 'assets/img/cards/4-stars/Birthday_-Museum-_Aoyagi_Tsubaki.png',
         rarity: 4,
         trained: 'assets/img/cards/4-stars/Birthday_-Museum-_Aoyagi_Tsubaki_T.png'
     },
     {
-        id: 12,
+        id: 13,
         name: 'Canary In The Sky',
         img: 'assets/img/cards/4-stars/Canary_in_the_Sky.png',
         rarity: 4,
         trained: 'assets/img/cards/4-stars/Canary_in_the_Sky_T.png'
     },
     {
-        id: 13,
+        id: 14,
         name: 'Moonlight Diva',
         img: 'assets/img/cards/4-stars/Moonlight_Diva.png',
         rarity: 4,
         trained: 'assets/img/cards/4-stars/Moonlight_Diva_T.png'
     },
     {
-        id: 14,
+        id: 15,
         name: 'Passionate Bystander',
         img: 'assets/img/cards/4-stars/Passionate_Bystander.png',
         rarity: 4,
         trained: 'assets/img/cards/4-stars/Passionate_Bystander_T.png'
     },
     {
-        id: 15,
+        id: 16,
         name: 'Shana And The Blue Flame Hunter',
         img: 'assets/img/cards/4-stars/Shana_and_the_Blue_Flame_Hunter.png',
         rarity: 4,
         trained: 'assets/img/cards/4-stars/Shana_and_the_Blue_Flame_Hunter_T.png'
     },
     {
-        id: 16,
+        id: 17,
         name: 'The Flower That Shines In The Moon\'s Light',
         img: 'assets/img/cards/4-stars/The_Flower_That_Shines_in_the_Moon\'s_Light.png',
         rarity: 4,
-        trained: 'assets/img/cards/4-stars/The_Flower_That_Shines_in_the_Moon\'s_Light.png'
+        trained: 'assets/img/cards/4-stars/The_Flower_That_Shines_in_the_Moon\'s_Light_T.png'
     },
     {
-        id: 17,
+        id: 18,
         name: 'Wavering Solo Camellia',
         img: 'assets/img/cards/4-stars/Wavering_Solo_Camellia.png',
         rarity: 4,
         trained: 'assets/img/cards/4-stars/Wavering_Solo_Camellia_T.png'
     },
     {
-        id: 18,
+        id: 19,
         name: 'White Rose Diva',
         img: 'assets/img/cards/4-stars/White_Rose_Diva.png',
         rarity: 4,
         trained: 'assets/img/cards/4-stars/White_Rose_Diva_T.png'
     },
     {
-        id: 19,
+        id: 20,
         name: 'Wistful Blue Moon',
         img: 'assets/img/cards/4-stars/Wistful_Blue_Moon.png',
         rarity: 4,
         trained: 'assets/img/cards/4-stars/Wistful_Blue_Moon_T.png'
     },
     {
-        id: 20,
+        id: 21,
         name: 'You Can\'t Veto It',
         img: 'assets/img/cards/4-stars/You_Can\'t_Veto_It.png',
         rarity: 4,
         trained: 'assets/img/cards/4-stars/You_Can\'t_Veto_It_T.png'
     },
     {
-        id: 21,
+        id: 22,
+        name: 'Evanescent_Avenger_of_Solitude',
+        img: 'assets/img/cards/4-stars/Evanescent_Avenger_of_Solitude.png',
+        rarity: 4,
+        trained: 'assets/img/cards/4-stars/Evanescent_Avenger_of_Solitude_T.png'
+    },
+    {
+        id: 23,
         name: '1st Anniversary',
         img: 'assets/img/cards/sp/1st_Anniversary.png',
         rarity: 5,
+        trained: null
+    },
+    {
+        id: 24,
+        name: 'At Your Service',
+        img: 'assets/img/cards/cameo-appearances/At_Your_Service.png',
+        rarity: 6,
         trained: null
     },
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,21 @@ cards.map((card, index) => {
     cardItem.querySelector('.card-img-top').alt = card.name;
     cardItem.querySelector('.card-title').innerHTML = card.name;
 
+    let active = false;
+
     if (card.trained) {
-        cardItem.querySelector('img').addEventListener('mouseover', () => {
-            cardItem.querySelector('.card-img-top').src = card.trained;
+        cardItem.querySelector('.switch').addEventListener('click', () => {
+            active = !active;
+            let cardImg = active ? card.trained : card.img;
+            cardItem.querySelector('.card-img-top').src = cardImg;
         });
-        cardItem.querySelector('img').addEventListener('mouseout', () => {
-            cardItem.querySelector('.card-img-top').src = card.img;
-        });
+    } else {
+        cardItem.querySelector('.switch').style.display = 'none';
+    }
+
+    if (card.rarity === 6) {
+        cardItem.querySelector('.card-title').style.color = 'darkblue';
+        cardItem.querySelector('.card-title').style.border = 'solid 1px black';
     }
 
     getElement('.cards-area ').append(cardItem);
